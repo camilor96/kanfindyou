02b0f925fc4996450035b2ff7843a32a339d9fb5
Camilo Rodríguez <juancamilo9607@gmail.com>
2020-11-03 23:17:57 -0500

    Primer commit con bootstrap y jquery

---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "./Navbar.css"
+
+function NavBar(){
+    return(
+        <div className="o-navBar">
+            <nav class="o-nav">
+            <div class="o-img-container">
+                <h1>KanFindYou</h1>
+            </div>
+        <div class="o-links-container">
+            <a class="o-link" href="#">Inicio</a>
+            <a class="o-link" href="#">Programa</a>
+            <a class="o-link" href="#">Nosotros</a>
+        </div>
+        <div class="o-buttons-container">
+            <button class="o-button">Iniciar sesión</button>
+            <button class="o-button o-button-call-to-action">Regístrate</button>
+
+        </div>
+        <div class="o-mobile-nav">
+            <button class="o-button">Menú</button>
+
+        </div>
+    </nav>
+        </div>
+    )
+}
+
+export default NavBar;
